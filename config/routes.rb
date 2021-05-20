f94d816c34794e4f178c6a972464913bc51095e3
Vojtech Matous <vojtech.matous@gmail.com>
2021-05-20 15:43:50 +0200

    rails models routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root to: 'pages#home'
+
+  resources :lists do # primary instance, has many movies and bookmarks
+    resources :bookmarks, only: [:new, :create] # we need only
+  end
+
+  resources :bookmarks, only: :destroy
 end
+
